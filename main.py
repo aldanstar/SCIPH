73dd34f2ca4c6c44598f58959dcec5f42f21d402
aldanstar <aldanstar@gmail.com>
2020-12-16 20:17:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+ #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#-------------------------------------------------------------------------------
+
+from app import Application
+
+def main():
+    app=Application()
+    app.application_init()
+
+if __name__ == '__main__':
+    main()
